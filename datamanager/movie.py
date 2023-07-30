010f011ccde47704e3fd261643c6f84914f5cfd3
Mete Durlu <meted@linux.ibm.com>
2023-07-30 17:13:42 +0200

    datamanager/movie: add ease of use api calls
    Signed-off-by: Mete Durlu <meted@linux.ibm.com>

--- datamanager/movie.py
+++ datamanager/movie.py
@@ -2,12 +2,24 @@ class movie:
     #
     # Movie class holds necessary information about the 
     #
-    def __init__(self, name, director, year, grd1 = 0, grd2 = 0):
+    def __init__(self, name, director, year:int, grd1:int = 0, grd2:int = 0):
         self.name = name
         self.director = director
         self.year = year
         self.grd1 = grd1
         self.grd2 = grd2
+
+    def from_dict(data_dict):
+        new_movie = movie(data_dict["name"],
+                          data_dict["director"],
+                          int(data_dict["year"]),
+                          int(data_dict["grd1"]),
+                          int(data_dict["grd2"])
+                          )
+        return new_movie
+
+    def get_fieldnames():
+        return ["name", "director", "year", "grd1", "grd2"]
 
     def get_avg_grade(self):
         return int((self.grd1 + self.grd2) / 2 )
@@ -21,7 +33,7 @@ class movie:
         self.grd1 = grd1
         self.grd2 = grd2
     
-    def get_info_as_dict(self):
+    def as_dict(self):
         return {"name": self.name,
                 "director": self.director,
                 "year": self.year,
