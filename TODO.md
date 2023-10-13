39269b68f92025142ace993ac9fc29407b303e31
Mete Durlu <meted@linux.ibm.com>
2023-10-13 20:24:13 +0200

    datamanager/movie: modify movie fields
    Movie no longer holds director name and year field has been replaced
    with date field.
    
    Signed-off-by: Mete Durlu <meted@linux.ibm.com>

--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
 ## 1) Implement Class Structure
 > movie manager (file, movies)
->> movie (id, title, year, r1, r2)
+>> movie (id, title, date, summary, r1, r2)
 >> file (name, location)
+
+date fmt (dd.mm.yyyy)
--- datamanager/movie.py
+++ datamanager/movie.py
@@ -1,32 +1,36 @@
+import datetime as dt
+
 class movie:
     #
     # Movie class holds necessary information about the 
     #
-    def __init__(self, name, director, year:int, grd1:int = 0, grd2:int = 0):
+    def __init__(self, name, date:dt.date, summary, grd1:int = 0, grd2:int = 0):
         self.name = name
-        self.director = director
-        self.year = year
+        self.date = date
+        self.summary = summary
         self.grd1 = grd1
         self.grd2 = grd2
 
     def from_dict(data_dict):
+        movie_date = data_dict["date"].split("/")
         new_movie = movie(data_dict["name"],
-                          data_dict["director"],
-                          int(data_dict["year"]),
+                          dt.date(movie_date[2], movie_date[1], movie_date[0]),
+                          data_dict["summary"],
                           int(data_dict["grd1"]),
                           int(data_dict["grd2"])
                           )
         return new_movie
 
     def get_fieldnames():
-        return ["name", "director", "year", "grd1", "grd2"]
+        return ["name", "date", "summary", "grd1", "grd2"]
 
     def get_avg_grade(self):
         return int((self.grd1 + self.grd2) / 2 )
 
     def print_info(self):
         print(f"{self.name} ({self.year}) :", )
-        print(f"- {self.director}")
+        print(f"- {self.date}")
+        print(f"- {self.summary}")
         print(f"Avg Grade: {self.get_avg_grade()}")
     
     def review(self, grd1 = 5, grd2 = 5):
@@ -35,7 +39,7 @@ class movie:
     
     def as_dict(self):
         return {"name": self.name,
-                "director": self.director,
-                "year": self.year,
+                "date": self.date,
+                "summary": self.summary,
                 "grd1": self.grd1,
                 "grd2": self.grd2}
